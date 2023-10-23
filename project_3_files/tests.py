d9362acf4f614762907299e0305be652e2bcd692
skinwalking-frog <146015517+skinwalking-frog@users.noreply.github.com>
2023-10-22 18:42:18 -0700

    made a very basic test

--- project_3_files/tests.py
+++ project_3_files/tests.py
@@ -4,3 +4,12 @@ import class_ts as TS
 """dont know what's going on with Travis's code and he's
 not available so im only testing the classes I made -Oliver Rothe"""
 
+#not really sure how to test any of my class attributes or functions.
+
+oven = OR.oven(3)
+
+if oven.rack == "bottom":
+    print("passed")
+
+#^^^^^^ best attempt I have at a test
+
