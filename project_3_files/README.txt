4b45334c9beea9453dd395246a70681685b7ce05
skinwalking-frog <146015517+skinwalking-frog@users.noreply.github.com>
2023-10-22 18:50:06 -0700

    Added a README

---
+++ project_3_files/README.txt
@@ -0,0 +1,3 @@
+Oliver Rothe and Travis Santos.
+
+Travis was around for one night to write a pizza class and then havent heard from him since, I wrote everything else :/ -Oliver Rothe
--- project_3_files/tests.py
+++ project_3_files/tests.py
@@ -2,7 +2,8 @@ import Classes_OR as OR
 import class_ts as TS
 
 """dont know what's going on with Travis's code and he's
-not available so im only testing the classes I made -Oliver Rothe"""
+not available so im not going to mess with his code and only test
+ the classes I made -Oliver Rothe"""
 
 #not really sure how to test any of my class attributes or functions.
 
