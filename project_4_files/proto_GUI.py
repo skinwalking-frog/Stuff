7dc6dec3e37df8b590caa04650ccc5be4f9f7cb8
skinwalking-frog <146015517+skinwalking-frog@users.noreply.github.com>
2023-10-25 18:49:40 -0700

    broke code

--- project_4_files/proto_GUI.py
+++ project_4_files/proto_GUI.py
@@ -10,15 +10,17 @@ class GUI_1():
     with text from textbox when button is pressed"""
     def __init__(self):
         self.root = tk.Tk()
+        self.root.geometry("600x400")
+        self.user_in = tk.StringVar(master=self.root)
         self.label = tk.Label(master=self.root, text="empty")
-        self.entry = tk.Entry(master=self.root)
+        self.entry = tk.Entry(master=self.root, textvariable=self.user_in)
         self.button = tk.Button(master=self.root, text="set", command=self.set_lab())
         self.label.pack()
         self.entry.pack()
         self.button.pack()
 
     def set_lab(self):
-        self.label.config(text=self.entry.get)
+        self.label.config(text=self.user_in.get())
 
     def run_GUI(self):
         self.root.mainloop()
