e8da7c9ce961849417aa9fd9658202b57cd1e345
skinwalking-frog <146015517+skinwalking-frog@users.noreply.github.com>
2023-10-25 21:46:21 -0700

    fixed code

--- project_4_files/proto_GUI.py
+++ project_4_files/proto_GUI.py
@@ -14,7 +14,7 @@ class GUI_1():
         self.user_in = tk.StringVar(master=self.root)
         self.label = tk.Label(master=self.root, text="empty")
         self.entry = tk.Entry(master=self.root, textvariable=self.user_in)
-        self.button = tk.Button(master=self.root, text="set", command=self.set_lab())
+        self.button = tk.Button(master=self.root, text="set", command=self.set_lab)
         self.label.pack()
         self.entry.pack()
         self.button.pack()
@@ -25,6 +25,6 @@ class GUI_1():
     def run_GUI(self):
         self.root.mainloop()
 
-window = GUI_1()
-
-window.run_GUI()
+if __name__ == "main":
+    window = GUI_1()
+    window.run_GUI()
